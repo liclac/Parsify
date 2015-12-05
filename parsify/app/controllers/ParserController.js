25155b1a661ff12c3134e5128b0f8c58d0a1debf
uppfinnarn <uppfinnarn@gmail.com>
2015-12-05 17:33:21 +0100

    Export data straight to the scope

--- parsify/app/controllers/ParserController.js
+++ parsify/app/controllers/ParserController.js
@@ -1,6 +1,9 @@
 app.controller('ParserController', ['$scope', function($scope) {
 	$scope.$on('actUpdate', function(e, state) {
-		$scope.data = state;
+		// $scope.data = state;
+		for (var key in state) {
+			$scope[key] = state[key];
+		}
 		$scope.$apply();
 	});
 }]);
