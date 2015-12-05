9d6ff243e15bfd00fb4189beab96b30efd7bdd63
uppfinnarn <uppfinnarn@gmail.com>
2015-12-05 20:39:41 +0100

    num and pct were redundant

--- parsify/app/filters/num.js
+++ parsify/app/filters/num.js
@@ -1,6 +1,9 @@
 app.filter('num', function() {
 	return function(val, decimals) {
 		decimals = (decimals !== undefined ? decimals : 2);
+		if (typeof(val) === "string") {
+			val = parseFloat(val);
+		}
 		if (val == NaN) {
 			return "---";
 		}
--- parsify/app/filters/pct.js
+++ parsify/app/filters/pct.js
@@ -1,10 +1,5 @@
 app.filter('pct', function() {
-	return function(val, decimals) {
-		decimals = (decimals != undefined ? decimals : 2);
-		if (val == NaN) {
-			return "---";
-		}
-		var pct = 100 * val;
-		return (pct ? pct : 0.0).toFixed(decimals);
+	return function(val) {
+		return 100 * val;
 	}
 });
