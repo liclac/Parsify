5e44b5fbe5b501dd9a8e23db2e66585e053e8088
uppfinnarn <uppfinnarn@gmail.com>
2015-12-09 14:47:20 +0100

    port of deadYatsuEffect as a plugin/directive

---
+++ plugins/highlight-deaths/script.js
@@ -0,0 +1,15 @@
+app.directive('highlightDeaths', function() {
+	return {
+		link: function(scope, e, attrs) {
+			var charKey = attrs.highlightDeaths || 'char';
+			scope.$watch(charKey + '.deaths', function(old, val) {
+				var className = 'highlight-deaths-has-deaths';
+				if (val > 0) {
+					e.addClass(className);
+				} else {
+					e.addClass(className);
+				}
+			});
+		}
+	};
+});
